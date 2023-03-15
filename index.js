0ee06fbb9d7430b34f92cca661a6488af86e980c
Aizen <iansky3311@gmail.com>
2023-03-15 04:43:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dfb){
+    if (dfb >42){
+        return dfb-42;
+    } else{
+        return 42- dfb;
+    }
+}
+function distanceFromHqInFeet  (dfb){
+    return distanceFromHqInBlocks(dfb)*264
+}
+function distanceTravelledInFeet(start, destination){
+    if (start >destination) {
+        return (start -destination)*264
+    } else return (destination-start)*264;
+}
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet (start, destination)<=400){
+        return 0;
+    } else if(distanceTravelledInFeet(start, destination)> 400 && distanceTravelledInFeet(start, destination)<=2000){
+        return 0.02*(distanceTravelledInFeet(start, destination)-400);
+    } else if(distanceTravelledInFeet(start, destination)>2000 &&distanceTravelledInFeet(start,destination)<=2500){
+        return 25
+    } else if(distanceTravelledInFeet(start, destination)>2500){
+        return "cannot travel that far"
+    }
+}
